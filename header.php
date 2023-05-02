d276812d9cba16d6bee119ad7eef46ee9616be82
siposgfx <zoltan.siposko121@gmail.com>
2023-05-02 22:15:36 +0200

    Back to normal

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Bootstrap demo testuu</title>
+	<title>Bootstrap demo test</title>
 	<?php wp_head(); ?>
 </head>
 <body>
