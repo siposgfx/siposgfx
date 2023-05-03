cc8ccd5f40e14b5cb0689a877ab37002331194ab
siposgfx <zoltan.siposko121@gmail.com>
2023-05-03 19:09:06 +0200

    Added favicon

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Bootstrap demo test</title>
+	<title>siposgfx | grafický dizajnér</title>
 	<?php wp_head(); ?>
 </head>
 <body>
