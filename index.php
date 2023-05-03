2d43d287412e496e9945904fe81eb0f748504d1b
siposgfx <zoltan.siposko121@gmail.com>
2023-05-03 19:54:07 +0200

    added logo

--- index.php
+++ index.php
@@ -14,14 +14,12 @@
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-    <ul class="navbar-nav my-2 my-lg-">
+  <a class="navbar-brand" href="#">
+    <img src="/img/logo.png" width="30" height="30" alt="">
+  </a>
+    <ul class="navbar-nav my-2 my-lg-0">
     <li class="nav-item">
-        <a class="nav-link " href="mailto:work@siposgfx.sk">Disabled</a>
+        <a class="nav-link " href="mailto:work@siposgfx.sk">work@siposgfx.sk</a>
       </li>
     </ul>
   </div>
