1007b0b76fc9366b3765dc2ee0a42c663c2a70de
siposgfx <zoltan.siposko121@gmail.com>
2023-05-04 16:32:13 +0200

    ads

--- functions.php
+++ functions.php
@@ -17,15 +17,15 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
 
 //Set the branch that contains the stable release.
 $myUpdateChecker->setBranch('main');
-
-
 /**
  * Load Bootstrap 5
  *
  * @since 0.0.2
  */
+
 function load_bootstrap_scripts() {
 	wp_enqueue_style( 'bs-5-css', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '5.3.0-alpha3', 'all' );
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
 	wp_enqueue_script( 'bs-5-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '5.3.0-alpha3', false );
 }
 add_action( 'wp_enqueue_scripts', 'load_bootstrap_scripts' );
--- index.php
+++ index.php
@@ -4,27 +4,44 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>siposgfx | grafický dizajner</title>
-	<?php get_header(); ?>
+
+	<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
+    <?php get_header(); ?>
 </head>
 <body class="cstm-bg">
    
 <nav class="navbar navbar-expand-lg navbar-light">
-<div class="container">
+<div class="container-lg">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
   <a class="navbar-brand" href="#">
-    <img src="siposgfx.sk/web/wp-content/themes/siposgfx/img/logo.png" width="30" height="30" alt="">
+    <img src="https://www.siposgfx.sk/wp-content/uploads/2023/05/logo.png"  height="30" alt="">
   </a>
    
   </div>
   <ul class="navbar-nav my-2 my-lg-0">
     <li class="nav-item">
-        <a class="email-txt nav-link" href="mailto:work@siposgfx.sk">work@siposgfx.sk</a>
+        <a class="email-txt text-decoration-none" href="mailto:work@siposgfx.sk">work@siposgfx.sk</a>
       </li>
     </ul>
 </nav>
+<div class="container">
+<div class="row align-items-center">
+                        <div class="col-md-6">
+                            <h1 class="fw-bold">Zdravím, <br> som Zoltán. </h1>
+                                        <p class="text-headline">Som grafický dizajnér na voľnej nohe. <br> Vytváram moderný digitálny dizajn <br> na mieru.</p>
+                                        <div class="column">
+                                        <a class="links-custom-btn fw-bold mr-2" href="#" role="button">Služby <i class="fa-solid fa-arrow-turn-up" style="color: #4E4E4E;"></i></a>
+                                        <a class="links-custom-btn fw-bold" href="#" role="button">Portfólio <i class="fa-solid fa-arrow-turn-up" style="color: #4E4E4E;"></i> </a> 
+                                        </div>
+                        </div>
+                        <div class="col-md-6 how-img">
+                            <img src="https://www.siposgfx.sk/wp-content/uploads/2023/05/heading-photo.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+</div>
 </div>
 </body>
 
